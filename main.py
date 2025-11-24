80e6eb1c3de916f6a2cbaf189a091adfde8592c5
akamah1396 <hastaga196@gmail.com>
2025-11-24 14:03:25 -0800

    main module updated

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+import authkey
+
+BOT_TOKEN = authkey.BOT_TOKEN
+
+
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("سلام! ربات با Aiogram آماده است 😊")
+
+@dp.message()
+async def echo_handler(message: Message):
+    await message.answer(f"تو گفتی: {message.text}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
