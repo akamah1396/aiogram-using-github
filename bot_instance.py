d0def77a9363f43037fa5df428bf79b2efd5ce67
akamah1396 <hastaga196@gmail.com>
2025-11-25 23:42:52 -0800

    bot development aproach changed

---
+++ bot_instance.py
@@ -0,0 +1,3 @@
+from aiogram import Bot
+from authkey import BOT_TOKEN
+bot = Bot(token=BOT_TOKEN)
--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
 from aiogram.filters import Command
-import authkey
+#from aiogram.types import Message
+from bot_instance import bot
 
-BOT_TOKEN = authkey.BOT_TOKEN
-
-
-
-bot = Bot(token=BOT_TOKEN
-          )
+my_bot = bot
 dp = Dispatcher()
+router = Router()
 
+@router.message(Command("start"))
+async def start_message(msg):
+    await msg.answer("ربات شما فعال شد.")
 
+dp.include_router(router)
 
-@dp.message(Command("start"))
-async def cmd_start(message):
-    await message.answer(f" سلام { message.from_user.first_name}! ربات با Aiogram آماده است \n حذف اضافات انجام شد! 😊")
-
-@dp.message()
-async def echo_handler(message):
-    await message.answer(f"تو گفتی: {message.text}")
 
 async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
+
