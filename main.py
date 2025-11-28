abcfd614a8eecacd6735c2e0516fbdcca508741c
akamah1396 <hastaga196@gmail.com>
2025-11-28 15:38:23 -0800

    rrrrre

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def reply_start(message: Message)->None:
     await message.answer(f"سلام {message.from_user.first_name} \n ربات شما فعال شد. ")
-def register_routers(dp):
-    dp.include_router(send_photo_router)
+
+@dp.message(Command("photo"))
+async def reply_start(message: Message)->None:
+    await message.answer_photo("resources\images\gff.png")
+
 async def main():
-    register_routers(dp)
+    
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
