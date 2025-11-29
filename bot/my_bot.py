781c450f5a47b0bf8acc4730b5b9f3e6ff29a896
akamah1396 <hastaga196@gmail.com>
2025-11-29 14:19:00 -0800

    secre

--- bot/my_bot.py
+++ bot/my_bot.py
@@ -1,3 +1,6 @@
 from aiogram import Bot
-from bot.config import BOT_TOKEN
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
+BOT_TOKEN=os.getenv("BOT_TOKEN")
 bot = Bot(token= BOT_TOKEN)
