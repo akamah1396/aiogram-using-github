e249521b072ff8e41aa43c929fb1c95ca4d4d781
akamah1396 <hastaga196@gmail.com>
2025-11-28 16:00:24 -0800

    gtyttty

--- bot/handlers/send_photo.py
+++ bot/handlers/send_photo.py
@@ -7,5 +7,5 @@ send_photo_router = Router()
 
 @send_photo_router.message(Command("photo"))
 async def send_photos(message: Message):
-    photo = FSInputFile("resources\\images\\gff.png")
+    photo = FSInputFile("resources/images/gff.png")
     await message.answer_photo(photo=photo,caption="اینم از عکست!")
