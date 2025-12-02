6e0595f2ad2663dd54442a34ccdb8521cc572a4e
akamah1396 <hastaga196@gmail.com>
2025-12-02 14:11:59 -0800

    lkmjnh

--- main.py
+++ main.py
@@ -52,13 +52,12 @@ async def language_handler(message: Message, state:FSMContext)-> None:
     data = await state.update_data(language = message.text)
     data = await state.get_data()
     state.clear()
+async def show_summary(message: Message, data: dict, positive: bool = True) -> None:
+    name = data["name"]
+    language = data["language"]
+    text = f"I'll keep in mind that, {html.quote(name)},your preferred language is: {language}"
 
-    async def show_summary(message: Message, data: dict, positive: bool = True) -> None:
-        name = data["name"]
-        language = data["language"]
-        text = f"I'll keep in mind that, {html.quote(name)},your preferred language is: {language}"
-
-        await message.answer(text=text)  
+    await message.answer(text=text)  
 
 
 
