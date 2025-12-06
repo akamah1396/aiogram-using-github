c3557aef46489b48f9885f44a04682f1a1330b10
akamah1396 <hastaga196@gmail.com>
2025-12-06 13:00:38 -0800

    qqaass

--- test.py
+++ test.py
@@ -8,5 +8,6 @@ from typing import Callable, Dict, Any
 class PeyghamMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
         data["harchi"] = "دیتای میان افزار"
+        await event.answer("این نوشته قبل از هندلر فرستاده شد.")
         result = await handler(event,data)
         print("after mid is running...")
