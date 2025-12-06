c4fb751de7e777f1fd55cff29f0ccba97beeb398
akamah1396 <hastaga196@gmail.com>
2025-12-06 13:46:28 -0800

    iuoiu

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ class PeyghamMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data)->Any:
         data["harchi"] = "دیتای میان افزار"
         await event.answer("این نوشته قبل از هندلر فرستاده شد.")
-        return
         result = await handler(event,data)
-        print("after mid is running...")
+        await event.answer("بعد از هندلر اجرا شد این.")
         return result
