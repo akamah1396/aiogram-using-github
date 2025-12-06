1084de8adba6d50ae99321e4894d6e59133faf38
akamah1396 <hastaga196@gmail.com>
2025-12-06 14:05:46 -0800

    trfd

--- bot/handlers/mid_router.py
+++ bot/handlers/mid_router.py
@@ -7,5 +7,12 @@ middleware_router = Router()
 middleware_router.message.middleware(PeyghamMiddleware())
 
 @middleware_router.message(Command("name"))
-async def midHandler(message: Message, harchi: str = None):
-    await message.answer(f"{harchi}")
+async def midHandler(message: Message, harchi: str = None, access: str = "denied"):
+    if access == "denied":
+        await message.answer("شما مجاز نیستید")
+       
+    elif access == "approved":
+        await message.answer("یو ار اتنتیکیتد.")
+    
+    else:
+        await message.answer("سیستم با خطا مواجه شد.")
--- test.py
+++ test.py
@@ -9,6 +9,7 @@ class PeyghamMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data)->Any:
         data["harchi"] = "دیتای میان افزار"
         await event.answer("این نوشته قبل از هندلر فرستاده شد.")
+        data["access"] = "approved"
         result = await handler(event,data)
         await event.answer("بعد از هندلر اجرا شد این.")
         return result
